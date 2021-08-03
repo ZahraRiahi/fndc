edc5da5f0d4927eefff51c033f72c7bbf670a744
f.ashourinia <1997!Farzan>
2021-08-03 17:26:02 +0430

    FIN-268(8/3/2021)

--- app/src/main/java/ir/demisco/cfs/app/web/controller/FinancialLedgerTypeController.java
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/FinancialLedgerTypeController.java
@@ -1,11 +1,15 @@
 package ir.demisco.cfs.app.web.controller;
 
 import ir.demisco.cfs.model.dto.response.FinancialLedgerTypeDto;
+import ir.demisco.cfs.model.dto.response.FinancialLedgerTypeResponse;
 import ir.demisco.cfs.service.api.FinancialLedgerTypeService;
-import ir.demisco.cloud.core.security.util.SecurityHelper;
+import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
+import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,11 +24,16 @@ public class FinancialLedgerTypeController {
         this.financialLedgerTypeService = financialLedgerTypeService;
     }
 
-    @PostMapping("/Get")
+    @GetMapping("/Get")
     public ResponseEntity<List<FinancialLedgerTypeDto>> responseEntity() {
 
 //        Long organizationId = SecurityHelper.getCurrentUser().getOrganizationId();
         return ResponseEntity.ok(financialLedgerTypeService.getFinancialLedgerType(100L));
 
+    }
+
+    @PostMapping("/List")
+    public ResponseEntity<DataSourceResult> financialLedgerTypeList(@RequestBody DataSourceRequest dataSourceRequest) {
+      return ResponseEntity.ok(financialLedgerTypeService.financialLedgerTypeList(dataSourceRequest));
     }
 }
