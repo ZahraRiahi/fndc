8bbcbd322bcdb96c2f6e719a59baa94d1ab6b4ec
f.ashourinia <f.ashourina@demisco.com>
2021-08-08 16:51:44 +0430

    error commit

--- service/src/main/java/ir/demisco/cfs/service/api/FinancialLedgerTypeService.java
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialLedgerTypeService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface FinancialLedgerTypeService {
     /**
-     * لیست انواع دفاتر مالی
+     * واکشی انواع دفاتر مالی
      *
      * @param organizationId
      * @return
