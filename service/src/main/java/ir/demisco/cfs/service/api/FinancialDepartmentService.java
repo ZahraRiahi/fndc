b6b4d3572a4c5e2edd1c318398a09e4090efda07
f.ashourinia <1997!Farzan>
2021-08-14 10:49:00 +0430

    Feature-FIN-283

--- service/src/main/java/ir/demisco/cfs/service/api/FinancialDepartmentService.java
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialDepartmentService.java
@@ -1,11 +1,10 @@
 package ir.demisco.cfs.service.api;
 
-import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
 
 
 public interface FinancialDepartmentService {
-    /**لیست انواع شعب مال
+    /**لیست انواع شعب مالی
      * @return
      */
     DataSourceResult financialDepartmentList();
