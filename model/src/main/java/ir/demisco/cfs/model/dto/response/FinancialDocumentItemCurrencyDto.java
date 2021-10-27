9712a8628896bee681aefa383ee74b3712b604f2
m.kazemi <m.kazemi@istd.com>
2021-10-27 14:55:15 +0330

    feature-fin-638

--- model/src/main/java/ir/demisco/cfs/model/dto/response/FinancialDocumentItemCurrencyDto.java
+++ model/src/main/java/ir/demisco/cfs/model/dto/response/FinancialDocumentItemCurrencyDto.java
@@ -1,9 +1,11 @@
 package ir.demisco.cfs.model.dto.response;
 
-import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.map.JsonSerializer;
-import org.codehaus.jackson.map.SerializerProvider;
+
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
