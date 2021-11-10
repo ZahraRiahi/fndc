69a825c23ab36bc61c708af9789b6aab57aa8f03
m.kazemi <m.kazemi@istd.com>
2021-11-10 13:59:02 +0330

    feature-fin-679

---
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialAccountService.java
@@ -0,0 +1,8 @@
+package ir.demisco.cfs.service.api;
+
+import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
+import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
+
+public interface FinancialAccountService {
+    DataSourceResult getFinancialDocument(DataSourceRequest dataSourceRequest);
+}
