fc37bb506226d0364bfaa84e2b7bc74f4761803d
m.kazemi <m.kazemi@istd.com>
2022-01-26 11:51:06 +0330

    feature-fin-1050

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialDocumentTypeRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialDocumentTypeRepository.java
@@ -6,16 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface FinancialDocumentTypeRepository extends JpaRepository<FinancialDocumentType,Long> {
+public interface FinancialDocumentTypeRepository extends JpaRepository<FinancialDocumentType, Long> {
 
     @Query("select fd " +
             "from FinancialDocumentType fd" +
             " where fd.organization.id=:organizationId" +
-            " and :isFlag is null or ((:searchStatusFlag=false and fd.activeFlag=true) or (:searchStatusFlag=true))" +
+            " and :isFlag is null or ((:searchStatusFlag=false and fd.activeFlag=true) or (:searchStatusFlag=true)) " +
+            " and (:financialSystem is null or " +
+            " fd.financialSystem.id = :financialSystemId)" +
             " and fd.deletedDate is null ")
-    List<FinancialDocumentType> findByOrganizationId(Long organizationId, Boolean searchStatusFlag, String isFlag);
+    List<FinancialDocumentType> findByOrganizationId(Long organizationId, Boolean searchStatusFlag, String isFlag, Object financialSystem, Long financialSystemId);
 
-    @Query(value="select count(t.id)" +
+    @Query(value = "select count(t.id)" +
             "  from fndc.financial_document_type t" +
             "  left join fndc.financial_config fc" +
             "    on fc.financial_document_type_id = t.id" +
@@ -26,6 +28,6 @@ public interface FinancialDocumentTypeRepository extends JpaRepository<Financial
             " where t.id = :financialDocumentTypeId" +
             "   and ( fd.financial_document_type_id = :financialDocumentTypeId or" +
             "        fdp.financial_document_type_id = :financialDocumentTypeId or" +
-            "         fc.financial_document_type_id = :financialDocumentTypeId)",nativeQuery = true)
+            "         fc.financial_document_type_id = :financialDocumentTypeId)", nativeQuery = true)
     Long getDocumentTypeIdForDelete(Long financialDocumentTypeId);
 }
