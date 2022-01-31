53e282dc24241ef6a015584a9a5e20e79413216c
m.kazemi <m.kazemi@istd.com>
2022-01-31 10:32:17 +0330

    feature-fin-1066

---
+++ service/src/main/java/ir/demisco/cfs/service/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package ir.demisco.cfs.service.repository;
+
+import ir.demisco.cfs.model.entity.CentricAccount;
+import ir.demisco.cloud.basic.model.entity.org.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+}
