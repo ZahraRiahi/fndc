4b01b9e4bf189c13ba0a449b01c30c08788bfcfd
f.ashourinia <1997!Farzan>
2022-02-20 08:17:09 +0330

    FIN-1122 at 8:14 in 2/20/2022

--- model/src/main/java/ir/demisco/cfs/model/dto/request/FinancialLedgerTypeGetRequest.java
+++
@@ -1,7 +0,0 @@
-package ir.demisco.cfs.model.dto.request;
-
-import java.util.List;
-
-public class FinancialLedgerTypeGetRequest {
-   FinancialSecurityFilterRequest financialSecurityFilterRequests
-}
