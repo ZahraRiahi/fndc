650de78b0e139011199f72dfa6e689e09f37e14a
m.kazemi <m.kazemi@istd.com>
2022-03-07 14:54:33 +0330

    feature-fin-1165

--- model/src/main/java/ir/demisco/cfs/model/dto/request/FinancialDocumentTypeRequest.java
+++ model/src/main/java/ir/demisco/cfs/model/dto/request/FinancialDocumentTypeRequest.java
@@ -1,10 +1,14 @@
 package ir.demisco.cfs.model.dto.request;
 
+import java.util.Map;
+
 public class FinancialDocumentTypeRequest {
     private Long organizationId;
     private Long financialSystemId;
     private Long searchStatusFlag;
     private SecurityModelRequest securityModelRequest;
+    private Long id;
+    Map<String, Object> paramMap;
 
     public Long getOrganizationId() {
         return organizationId;
@@ -36,5 +40,21 @@ public class FinancialDocumentTypeRequest {
 
     public void setSecurityModelRequest(SecurityModelRequest securityModelRequest) {
         this.securityModelRequest = securityModelRequest;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Map<String, Object> getParamMap() {
+        return paramMap;
+    }
+
+    public void setParamMap(Map<String, Object> paramMap) {
+        this.paramMap = paramMap;
     }
 }
--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialDocumentTypeRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialDocumentTypeRepository.java
@@ -2,6 +2,8 @@ package ir.demisco.cfs.service.repository;
 
 import ir.demisco.cfs.model.entity.FinancialDocumentType;
 import org.hibernate.jpa.TypedParameterValue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -67,4 +69,24 @@ public interface FinancialDocumentTypeRepository extends JpaRepository<Financial
             "        fdp.financial_document_type_id = :financialDocumentTypeId or" +
             "         fc.financial_document_type_id = :financialDocumentTypeId)", nativeQuery = true)
     Long getDocumentTypeIdForDelete(Long financialDocumentTypeId);
+
+    @Query(value = " SELECT FNDT.id," +
+            "       FNDT.Description," +
+            "       ACTIVE_FLAG," +
+            "       AUTOMATIC_FLAG," +
+            "       ORGANIZATION_ID," +
+            "       FINANCIAL_SYSTEM_ID," +
+            "       FNS.DESCRIPTION FINANCIALSYSTEM_DESCRIPTION" +
+            "  FROM fndc.FINANCIAL_DOCUMENT_TYPE FNDT" +
+            " INNER JOIN FNSC.FINANCIAL_SYSTEM FNS" +
+            "    ON FNDT.FINANCIAL_SYSTEM_ID = FNS.ID" +
+            " WHERE (:financialSystem is null or FNDT.FINANCIAL_SYSTEM_ID =:financialSystemId )" +
+            "  and EXISTS (SELECT 1" +
+            "          FROM fndc.DOCUMENT_TYPE_ORG_REL INER_ORG_REL" +
+            "         WHERE INER_ORG_REL.ORGANIZATION_ID = :organizationId" +
+            "           AND INER_ORG_REL.FINANCIAL_DOCUMENT_TYPE_ID = FNDT.ID" +
+            "           AND INER_ORG_REL.ACTIVE_FLAG = 1)" +
+            " and (:idObject is null or FNDT.id =:id )"
+            , nativeQuery = true)
+    Page<Object[]> financialDocumentType(Object financialSystem, Long financialSystemId, Long organizationId, Object idObject, Long id, Pageable pageable);
 }
