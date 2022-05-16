c3888ae206774081bd3a2c82b8e1f578be8bd3be
m.kazemi <m.kazemi@istd.com>
2022-05-16 09:43:53 +0430

    feature-fin-1312

--- service/src/main/java/ir/demisco/cfs/service/repository/ApplicationUserRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/ApplicationUserRepository.java
@@ -8,36 +8,10 @@ import java.util.List;
 
 public interface ApplicationUserRepository extends JpaRepository<User, Long> {
 
-    @Query(value = "   select distinct (u.id), u.user_name, u.nick_name" +
-            "  from sec.application_user u" +
-            "  join sec.organization_user ou" +
-            "    on ou.user_id = u.id" +
-            "   and ou.enabled_flg = 1" +
-            "   and ou.deleted_date is null" +
-            "  join sec.user_job uj" +
-            "    on uj.organization_user_id = ou.id" +
-            "   and uj.disable_date is null" +
-            "  left join org.department de" +
-            "    on de.id = uj.department_id" +
-            "  left join org.department dc" +
-            "    on dc.root_id = de.root_id" +
-            "   and dc.disable_date is null" +
-            "  join org.organization o" +
-            "    on o.id = ou.organization_id" +
-            "  join org.organization oc" +
-            "    on oc.root_id = o.root_id" +
-            "   and oc.disable_date is null" +
-            "  join org.org_position_type ps" +
-            "    on ps.id = uj.org_position_type_id" +
-            "    and ps.disable_date is null" +
-            "  join sec.permission p" +
-            "    on p.org_position_type_id = ps.id" +
-            "    and p.disable_date is null" +
-            "  join aps.operation op" +
-            "    on op.id = p.operation_id" +
-            "    and op.disable_date is null" +
-            " where u.user_name LIKE '%FNC_USER%'" +
-            "   and ps.code like '%MANAGER%' "
+    @Query(value = "   select a.id, a.user_name, a.nick_name " +
+            "  from fnsc.financial_user FNUS " +
+            " inner join SEC.APPLICATION_USER A " +
+            "    on FNUS.APPLICATION_USER_ID = A.ID "
             , nativeQuery = true)
     List<Object[]> getUserList();
 }
