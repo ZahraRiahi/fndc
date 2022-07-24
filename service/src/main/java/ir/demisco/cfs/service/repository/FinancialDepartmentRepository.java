00750ef513312f036ea1759a7bcd4b7c7c65b15a
m.kazemi <m.kazemi@istd.com>
2022-07-24 18:15:14 +0430

    feature-fin-1452

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialDepartmentRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialDepartmentRepository.java
@@ -2,9 +2,7 @@ package ir.demisco.cfs.service.repository;
 
 import ir.demisco.cfs.model.entity.FinancialDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 
 public interface FinancialDepartmentRepository extends JpaRepository<FinancialDepartment, Long> {
 
