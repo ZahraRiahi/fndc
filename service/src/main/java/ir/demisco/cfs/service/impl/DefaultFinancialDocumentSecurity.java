6c7287803e4966c32a53a3315bf239be5ef07709
m.kazemi <m.kazemi@istd.com>
2022-08-24 17:30:36 +0430

    feature-fin-1543

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialDocumentSecurity.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialDocumentSecurity.java
@@ -4,7 +4,6 @@ import ir.demisco.cfs.model.dto.request.FinancialDocumentSecurityInputRequest;
 import ir.demisco.cfs.service.api.FinancialDocumentHeaderService;
 import ir.demisco.cfs.service.api.FinancialDocumentSecurityService;
 import ir.demisco.cfs.service.api.FinancialSecurityService;
-import ir.demisco.cloud.core.middle.exception.RuleException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,18 +20,4 @@ public class DefaultFinancialDocumentSecurity implements FinancialDocumentSecuri
         return true;
     }
 
-    private void checkFinancialDocumentSecurity(FinancialDocumentSecurityInputRequest financialDocumentSecurityInputRequest) {
-        if (financialDocumentSecurityInputRequest.getSecurityModelRequest().getUserId() == null) {
-            throw new RuleException("لطفا شناسه ی کاربر را وارد نمایید.");
-        }
-        if (financialDocumentSecurityInputRequest.getSecurityModelRequest().getDepartmentId() == null) {
-            throw new RuleException("لطفا شناسه ی شعبه را وارد نمایید.");
-        }
-        if (financialDocumentSecurityInputRequest.getSecurityModelRequest().getActivityCode() == null) {
-            throw new RuleException("لطفا کد نوع فعالیت را وارد نمایید.");
-        }
-        if (financialDocumentSecurityInputRequest.getSecurityModelRequest().getInputFromConfigFlag() == null) {
-            throw new RuleException("لطفا فلگ تنظیمات را وارد نمایید.");
-        }
-    }
 }
