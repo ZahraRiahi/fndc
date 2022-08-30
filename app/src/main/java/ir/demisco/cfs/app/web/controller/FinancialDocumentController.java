c72279b518fd6e5bbcaaab119d0a40e0a97aa839
m.kazemi <m.kazemi@istd.com>
2022-08-30 16:06:11 +0430

    feature-fin-1572

--- app/src/main/java/ir/demisco/cfs/app/web/controller/FinancialDocumentController.java
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/FinancialDocumentController.java
@@ -1,6 +1,5 @@
 package ir.demisco.cfs.app.web.controller;
 
-import com.fasterxml.jackson.databind.util.ISO8601Utils;
 import ir.demisco.cfs.model.dto.request.FinancialDocumentTransferRequest;
 import ir.demisco.cfs.model.dto.request.FinancialPeriodLedgerStatusRequest;
 import ir.demisco.cfs.model.dto.response.FinancialCentricAccountDto;
@@ -22,7 +21,6 @@ import ir.demisco.cfs.service.api.TransferFinancialDocumentService;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
 import ir.demisco.cloud.core.security.util.SecurityHelper;
-import ir.demisco.core.utils.DateUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,12 +28,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.ParseException;
-import java.text.ParsePosition;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 
 @RestController
