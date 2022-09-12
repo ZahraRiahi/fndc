b2a2fdf716d1c126c62e0ae5458a40cf85fe7d30
m.kazemi <m.kazemi@istd.com>
2022-09-12 17:21:22 +0430

    feature-fin-1612

--- app/src/main/java/ir/demisco/cfs/app/web/controller/FinancialLedgerPeriodController.java
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/FinancialLedgerPeriodController.java
@@ -1,14 +1,19 @@
 package ir.demisco.cfs.app.web.controller;
 
 import ir.demisco.cfs.model.dto.request.FinancialLedgerPeriodRequest;
+import ir.demisco.cfs.model.dto.response.FinancialPeriodLedgerGetResponse;
 import ir.demisco.cfs.service.api.FinancialLedgerPeriodService;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api-financialLedgerPeriod")
@@ -29,5 +34,10 @@ public class FinancialLedgerPeriodController {
     @PostMapping("/Get")
     public ResponseEntity<DataSourceResult> financialLedgerPeriodList(@RequestBody DataSourceRequest dataSourceRequest) {
         return ResponseEntity.ok(financialLedgerPeriodService.getFinancialLedgerPeriodList(dataSourceRequest));
+    }
+
+    @GetMapping("/GetByPeriod/{financialPeriodId}")
+    public ResponseEntity<List<FinancialPeriodLedgerGetResponse>>  responseEntityFinancialDocumentItem(@PathVariable Long financialPeriodId) {
+        return ResponseEntity.ok(financialLedgerPeriodService.getFinancialGetByPeriod(financialPeriodId));
     }
 }
