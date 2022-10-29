eef6a19ebbb0cd1ff1fa1d7f3dfb0a14e6eba250
m.kazemi <m.kazemi@istd.com>
2022-10-29 17:16:30 +0330

    feature-fin-1721

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultLedgerPeriod.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultLedgerPeriod.java
@@ -211,6 +211,82 @@ public class DefaultLedgerPeriod implements LedgerPeriodService {
             throw new RuleException("وضعیت ماه بعد بسته است و امکان انجام عملیات وجود ندارد");
         }
 
+
+        String minDocNumber = financialDocumentRepository.findByDocumentIdAndLedgerMonth(financialLedgerCloseMonthInputRequest.getFinancialLedgerMonthId(),
+                financialLedgerCloseMonthInputRequest.getFinancialLedgerPeriodId(), financialLedgerCloseMonthInputRequest.getFinancialPeriodId(),
+                SecurityHelper.getCurrentUser().getOrganizationId());
+
+        Long minDocId = financialDocumentRepository.findByDocumentIdAndLedgerMonthMinDocId(SecurityHelper.getCurrentUser().getOrganizationId(), financialLedgerCloseMonthInputRequest.getFinancialPeriodId()
+                , financialLedgerCloseMonthInputRequest.getFinancialLedgerTypeId(), minDocNumber);
+
+        entityManager.createNativeQuery(" UPDATE FNDC.NUMBERING_FORMAT_SERIAL NFS " +
+                "      SET NFS.LAST_SERIAL = REPLACE((:minDocNumber)," +
+                " NFS.SERIAL_RESETER, " +
+                "               '') - 1  " +
+                "  WHERE ID IN" +
+                "       (SELECT NFS.ID" +
+                "          FROM fndc.FINANCIAL_DOCUMENT FD" +
+                "         INNER JOIN fndc.LEDGER_NUMBERING_TYPE LNT" +
+                "            ON LNT.FINANCIAL_LEDGER_TYPE_ID = FD.FINANCIAL_LEDGER_TYPE_ID" +
+                "           AND LNT.DELETED_DATE IS NULL" +
+                "         INNER JOIN fndc.FINANCIAL_NUMBERING_TYPE NT" +
+                "            ON NT.ID = LNT.FINANCIAL_NUMBERING_TYPE_ID" +
+                "           AND NT.TYPE_STATUS = 3" +
+                "           AND NT.DELETED_DATE IS NULL" +
+                "         INNER JOIN fndc.FINANCIAL_NUMBERING_FORMAT NF" +
+                "            ON NF.FINANCIAL_NUMBERING_TYPE_ID = NT.ID" +
+                "           AND NF.ORGANIZATION_ID = :organizationId " +
+                "           AND NF.DELETED_DATE IS NULL" +
+                "         INNER JOIN fndc.FINANCIAL_NUMBERING_FORMAT_TYPE NFT" +
+                "            ON NFT.ID = NF.NUMBERING_FORMAT_TYPE_ID" +
+                "           AND NFT.DELETED_DATE IS NULL" +
+                "          LEFT OUTER JOIN fndc.NUMBERING_FORMAT_SERIAL NFS" +
+                "            ON NFS.NUMBERING_FORMAT_ID = NF.ID" +
+                "           AND NFS.DELETED_DATE IS NULL" +
+                "         WHERE FD.ID = :minDocId " +
+                "           AND FD.DELETED_DATE IS NULL" +
+                "           AND SERIAL_RESETER =" +
+                "               REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(" +
+                "                                                               " +
+                "                                                               REPLACE(NFT.CODE," +
+                "                                                                       '$SRL'," +
+                "                                                                       '')" +
+                "                                                              ," +
+                "                                                               '$DAT6'," +
+                "                                                               TO_CHAR(TO_DATE(TO_CHAR(FD.DOCUMENT_DATE," +
+                "                                                                                       'mm/dd/yyyy')," +
+                "                                                                               'mm/dd/yyyy')," +
+                "                                                                       'yymmdd'," +
+                "                                                                       'NLS_CALENDAR=persian'))," +
+                "                                                       '$DAT'," +
+                "                                                       TO_CHAR(TO_DATE(TO_CHAR(FD.DOCUMENT_DATE," +
+                "                                                                               'mm/dd/yyyy')," +
+                "                                                                       'mm/dd/yyyy')," +
+                "                                                               'yyyymmdd'," +
+                "                                                               'NLS_CALENDAR=persian'))," +
+                "                                               '$LEG'," +
+                "                                               (SELECT LT.CODE" +
+                "                                                  FROM fndc.FINANCIAL_LEDGER_TYPE LT" +
+                "                                                 WHERE LT.ID =" +
+                "                                                       FD.FINANCIAL_LEDGER_TYPE_ID))," +
+                "                                       '$DEP'," +
+                "                                       (SELECT DP.CODE" +
+                "                                          FROM ORG.DEPARTMENT DP" +
+                "                                         WHERE DP.ID = FD.DEPARTMENT_ID))," +
+                "                               '$ORG'," +
+                "                               (SELECT OG.CODE" +
+                "                                  FROM FNDC.FINANCIAL_ORGANIZATION OG" +
+                "                                 WHERE OG.ORGANIZATION_ID = :organizationId))," +
+                "                       '$PRI'," +
+                "                       (SELECT PR.CODE" +
+                "                          FROM FNPR.FINANCIAL_PERIOD PR" +
+                "                         WHERE PR.ID = FD.FINANCIAL_PERIOD_ID))) "
+        ).setParameter("minDocNumber", minDocNumber)
+                .setParameter("organizationId", SecurityHelper.getCurrentUser().getOrganizationId())
+                .setParameter("minDocId", minDocId)
+                .executeUpdate();
+
+
         financialDocumentNumberRepository.getFinancialDocumentNumberByOrgAndPeriodId(financialLedgerCloseMonthInputRequest.getFinancialLedgerMonthId(),
                 SecurityHelper.getCurrentUser().getOrganizationId(), financialLedgerCloseMonthInputRequest.getFinancialLedgerTypeId(),
                 financialLedgerCloseMonthInputRequest.getFinancialPeriodId())
