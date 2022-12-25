a9075f2d2a58b94c21d37fab9d0a165d4839f855
m.kazemi <m.kazemi@istd.com>
2022-12-25 16:46:34 +0330

    feature-fin-1850

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialAccount.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialAccount.java
@@ -25,7 +25,6 @@ import ir.demisco.core.utils.DateUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
